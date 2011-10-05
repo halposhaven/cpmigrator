e86c7aa1f29cb01065c22f834cc8d77dd9d55938
jmuffett <jasonmuffett@gmail.com>
2011-10-05 02:50:38 -0400

    Fixed syntax error, and added a fix attempt if userdatadomains does not exist.
    1. Fixed syntax error with braces
    
    2. If /etc/userdatadomains does not exist, cpanel is likely having other problems.
       Attemps to run a upcp --force and checks again before giving up.

--- full-migration/scripts/testing-phase.sh
+++ full-migration/scripts/testing-phase.sh
@@ -5,13 +5,22 @@ unalias ls 2> /dev/null
 # If these files already exist, remove them
 [ -f /usr/local/apache/htdocs/hosts_file_entries.html ] && rm -f /usr/local/apache/htdocs/hosts_file_entries.html
 [ -f /usr/local/apache/htdocs/migration_test_urls.html ] && rm -f /usr/local/apache/htdocs/migration_test_urls.html
-[ -f /home/temp/testing_errors.txt } && rm -f /home/temp/testing_errors.txt
+[ -f /home/temp/testing_errors.txt ] && rm -f /home/temp/testing_errors.txt
 
 # Create accessible page for hosts file entries. (credit to jpurkis on this)
 if [[ -f /etc/userdatadomains ]]; then
 	cat /etc/userdatadomains|sed -e 's/:/ /g' -e 's/==/ /g'|while read sdomain user owner type maindomain docroot ip port;do echo "<br>"$ip $sdomain "www."$sdomain >> /usr/local/apache/htdocs/hosts_file_entries.html;done
 else
-	echo "/etc/userdatadomains does not exist. Hosts entries not created." >> /home/temp/testing_errors.txt
+	echo "Problem found. /ect/userdatadomains not found. Trying to fix ..."
+	/scripts/upcp --force > /dev/null 2>&1
+	if [[ -f /etc/userdatadomains ]]; then
+		echo "Problem fixed. Creating hosts file page ..."
+		sleep 2
+		cat /etc/userdatadomains|sed -e 's/:/ /g' -e 's/==/ /g'|while read sdomain user owner type maindomain docroot ip port;do echo "<br>"$ip $sdomain "www."$sdomain >> /usr/local/apache/htdocs/hosts_file_entries.html;done
+	else
+		echo "Problem not fixed. Unable to create hosts file entries."
+		echo "/etc/userdatadomains does not exist. Hosts entries not created." >> /home/temp/testing_errors.txt
+	fi
 fi
 
 # Add lwtest.html files to all primary accounts
